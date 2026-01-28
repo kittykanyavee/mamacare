f3628e43c0e0a5a8f76739a87cb57a834e16bacd
kittykanyavee <kanyavee.c@bcn.ac.th>
2026-01-28 23:31:00 +0700

    refactor:  improve Firebase config structure; add session middleware

--- src/app/firebase/client.ts
+++ src/app/firebase/client.ts
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBjD-dJ_N4IAOy1ViWnx6zNC41jP8RdYME",
-  authDomain: "mamacare-975e4.firebaseapp.com",
-  projectId: "mamacare-975e4",
-  storageBucket: "mamacare-975e4.firebasestorage.app",
-  messagingSenderId: "105719713004",
-  appId: "1:105719713004:web:1ad7da192241d5a88ce9c9",
-  measurementId: "G-RB5M20T70M"
+  apiKey: 'AIzaSyBjD-dJ_N4IAOy1ViWnx6zNC41jP8RdYME',
+  authDomain: 'mamacare-975e4.firebaseapp.com',
+  projectId: 'mamacare-975e4',
+  storageBucket: 'mamacare-975e4.firebasestorage.app',
+  messagingSenderId: '105719713004',
+  appId: '1:105719713004:web:1ad7da192241d5a88ce9c9',
+  measurementId: 'G-RB5M20T70M',
 };
 
 export const app = initializeApp(firebaseConfig);
